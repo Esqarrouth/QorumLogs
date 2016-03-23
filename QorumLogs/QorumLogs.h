cb3cb410c2f548ec9e8e66914ec0288c6766aacc
Christian Oberdörfer <christian.michael.oberdoerfer@gmail.com>
2016-03-23 10:15:03 +0100

    Added Xcode project for iOS framework
    An Xcode project was added to provide a framework for iOS, which can be imported easily into other projects. It may be necessary to modify the project to allow it being added to OS X or tvOS projects. This may also affect the podspec file.

---
+++ QorumLogs/QorumLogs.h
@@ -0,0 +1,19 @@
+//
+//  QorumLogs.h
+//  QorumLogs
+//
+//  Created by Christian Oberdörfer on 23.03.16.
+//  Copyright © 2016 goktugyil. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for QorumLogs.
+FOUNDATION_EXPORT double QorumLogsVersionNumber;
+
+//! Project version string for QorumLogs.
+FOUNDATION_EXPORT const unsigned char QorumLogsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <QorumLogs/PublicHeader.h>
+
+
