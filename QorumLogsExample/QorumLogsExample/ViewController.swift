b463309924470e8aebad9d13c1c41d8680fd8c58
Goktug Yilmaz <goktugyil@gmail.com>
2016-04-06 13:28:19 +0300

    Created example project

---
+++ QorumLogsExample/QorumLogsExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  QorumLogsExample
+//
+//  Created by Goktug Yilmaz on 4/6/16.
+//  Copyright © 2016 Goktug Yilmaz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
